a1af68a78ddfef7597126cafdfb951e8b749b8e7
Emiliano Somoza <emisomoza@hotmail.com>
2025-08-25 17:30:38 +1000

    change types

--- src/main/java/com/codewithmosh/store/dtos/ProductDto.java
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -3,12 +3,14 @@ package com.codewithmosh.store.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @Getter
 public class ProductDto {
     private Long id;
     private String name;
     private String description;
-    private Double price;
-    private String categoryId;
+    private BigDecimal price;
+    private Byte categoryId;
 }
